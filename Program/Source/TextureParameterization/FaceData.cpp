a01e827977fd95d96a59889b435530a979801372
YuanWeiChen16 <wficta137@gmail.com>
2022-08-30 18:24:31 +0800

    add File

--- Program/Source/TextureParameterization/FaceData.cpp
+++ Program/Source/TextureParameterization/FaceData.cpp
@@ -1,13 +1,49 @@
 #include"FaceData.h"
 
+//計算距離
+double FaceData::FaceDistance(FaceData A, FaceData B)
+{
+	//normal 一致
+	if (glm::length(A.realNormal - B.realNormal) < 0.0001)
+	{
+
+
+
+
+	}
+	else //normal 不一樣
+	{
+#pragma omp parallel for
+		for (int i = 0; i < A.pointcount; i++)
+		{
+#pragma omp parallel for
+			for (int j = 0; j < B.pointcount; j++)
+			{
+
+			}
+		}
+	}
+
+
+
+	return 0.0;
+}
+
+FaceData FaceData::FaceAdd(FaceData, FaceData)
+{
+	return FaceData();
+}
+
+FaceData FaceData::FaceAvg(FaceData, int)
+{
+	return FaceData();
+}
+
 double FaceData::operator-(FaceData& facedata)
 {
 	
 	return 0;
 }
-
-
-
 
 FaceData FaceData::operator+(FaceData& facedata)
 {
--- Program/Source/TextureParameterization/FaceData.h
+++ Program/Source/TextureParameterization/FaceData.h
@@ -9,6 +9,8 @@
 #include <string>
 #include <map>
 
+#include <omp.h>
+
 #include <Eigen/Sparse>
 class FaceData {
 public:
@@ -21,8 +23,17 @@ public:
 	std::vector<glm::vec3> Normal;
 	//平均normal
 	glm::vec3 realNormal;
-
+	
+	//點在圖上的UV位置，計算在圖上位置
+	std::vector<glm::vec2> UV;
 
 	double FaceData::operator-(FaceData& FaceData);
+	
+	//計算兩面的距離，需考慮多種情況
+	double FaceDistance(FaceData, FaceData);
+	//
+	FaceData FaceAdd(FaceData, FaceData);
+	FaceData FaceAvg(FaceData, int);
+
 	FaceData FaceData::operator+(FaceData& FaceData);
 };
